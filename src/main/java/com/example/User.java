998f1e6e4e45074f1b64a9e45eb2b68cb5887bbc
vitorbizarra <vitorbizarracontato@gmail.com>
2023-09-25 22:35:23 -0300

    :tada: init: First commit

---
+++ src/main/java/com/example/User.java
@@ -0,0 +1,43 @@
+package com.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+public class User {
+    public Connection conectarDB() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.Driver.Manager").newInstance();
+            String url = "jdbc:mysql://127.0.0.1/test?user=lopes&password=123";
+            conn = DriverManager.getConnection(url);
+        } catch (Exception e) {
+        }
+        return connection;
+    }
+
+    public String nome = "";
+    public boolean result = false;
+
+    public boolean verificarUsuario(String login, String senha) {
+        String sql = "";
+        Connection conn = conectarDB();
+
+        sql += "select nome from usuarios ";
+        sql += "where login = " + "'" + login + "'";
+        sql += " and senha = " + "'" + senha + "'";
+
+        try {
+            Statement st = conn.createStatement();
+            ResultSet = st.executeQuery(sql);
+
+            if (rs.next()) {
+                result = true;
+                nome = rs.getString("nome");
+            }
+        } catch (Exception e) {
+        }
+        return result;
+    }
+}
